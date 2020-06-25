070d14cadbf98533fee1cdb9dad14335ec413c47
Alvin Rombora <66435075+alvinshaita@users.noreply.github.com>
2020-06-26 02:36:22 +0300

    Update one.js

--- one.js
+++ one.js
@@ -1,3 +1,4 @@
 // alert("one");
 // window.open("http://www.google.com/");
-location.href = "http://www.google.com/";
+// location.href = "http://www.google.com/";
+location.href = "http://52.23.176.64:3000/";
